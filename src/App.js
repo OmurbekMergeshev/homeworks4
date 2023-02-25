d8fd8089b2248703b2872f17628a40ddadb92268
Omar <vip.mergeshev@gmail.com>
2023-02-25 11:36:32 +0600

    add homeworks4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Имя пользователя: ${this.state.username} Пароль: ${this.state.password}`);
+  };
+
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={this.handleSubmit}>
+      <label>
+        Username:
+        <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+      </label>
+      <label>
+        Password:
+        <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
+  }
 }
 
-export default App;
+
+export default LoginForm;
